c3d13117c745c872f7427020ecf9ad107839173e
Johnathon Crawford <47326832+Kodi125@users.noreply.github.com>
2020-03-23 17:05:12 +0000

    Add project files.

---
+++ Q1/Program.cs
@@ -0,0 +1,29 @@
+﻿/* #############################
+ * 
+ * Author: Johnathon Mc Grory
+ * Date :
+ * Description : printing to the console using arguements
+ * 
+ * ############################# */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            PrintMesageWithArguement("Happy Birthday", "Tommy");
+            PrintMesageWithArguement("Enjoy the day", "I'll see you soon");
+        }
+        static void PrintMesageWithArguement(string message, string message_two)
+        {
+            Console.WriteLine("\t\t**********{0}************{1}************\t\t", message, message_two);
+        }
+    }
+}
---
+++ nuggets/Program.cs
@@ -0,0 +1,43 @@
+﻿/* #############################
+ * 
+ * Author: Johnathon Mc Grory
+ * Date :
+ * Description : bool method validation
+ * 
+ * ############################# */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nuggets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the amount of nuggets: ");
+            int numNuggets = int.Parse(Console.ReadLine());
+            bool buyNuggets = CanBuyMcNuggets(numNuggets);
+            if (buyNuggets == true)
+            {
+                Console.WriteLine("Valid order");
+            }
+            else
+                Console.WriteLine("Invalid order");
+        }
+        static bool CanBuyMcNuggets(int numNuggets)
+        {
+
+            if ((numNuggets == 6) || (numNuggets == 9) || (numNuggets == 20))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
